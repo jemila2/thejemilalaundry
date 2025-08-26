b1f5e730e2ea016477a3498177dfebf9c4762615
jemila2 <abubakarjemiletu08@gmail.com>
2025-08-26 11:06:16 +0100

    Update App.jsx

--- laundryfr/src/App.jsx
+++ laundryfr/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import AdminRoute from './components/AdminRoute';
